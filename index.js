543abaee182913279d5f9d0952f9d7910caf229d
Mike <mclancy96@gmail.com>
2025-05-15 11:31:07 -0400

    Post request successful

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const submitData = (userName, userEmail) => {
+  const formData = {
+    name: userName,
+    email: userEmail,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.log(error.message);
+    });
+}
